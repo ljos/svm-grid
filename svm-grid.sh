4f7def774bc71c7707865556fb9e3481e64b85c0
Bjarte Johansen <bjarte.johansen@gmail.com>
2015-02-05 15:24:25 +0100

    Retry, don't log
    I am interested in trying to do the same computation again if it fails,
    not just loging failures.

--- svm-grid.sh
+++ svm-grid.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-LOGFILE=$(mktemp "XXXX.parallel.log")
-
 function unused {
     parallel --plain                                                \
              --sshloginfile ..                                      \
@@ -14,35 +12,33 @@ function unused {
 
 function exit_parallel {
     trap '' SIGINT SIGQUIT
-    parallel --plain                                \
-             --sshloginfile ..                      \
-             --nonall                               \
+    parallel --plain                                                \
+             --sshloginfile ..                                      \
+             --nonall                                               \
              'killall -q -u $USER svm-train'
-    rm "$LOGFILE"
 }
 
-trap 'echo "Ctrl-C detected.";                  \
-          exit_parallel;                        \
-          exit 130'                             \
+trap 'echo "Ctrl-C detected.";                                      \
+          exit_parallel;                                            \
+          exit 130'                                                 \
      SIGINT SIGQUIT
 
 DATA="$DOKTORGRAD/aggr/chunks.svm.bin"
 
-parallel --plain                                    \
-         --sshloginfile <(unused)                   \
-         --filter-hosts                             \
-         --joblog "$LOGFILE"                        \
-         --resume-failed                            \
-         --timeout 28800                            \
-         --tag                                      \
-         'nice svm-train -q                         \
-                         -m 1024                    \
-                         -h 0                       \
-                         -v 5                       \
-                         -c $(echo 2^{1} | bc -l)   \
-                         -g $(echo 2^{2} | bc -l)   \
-                 "'$DATA'"                          \
-              | sed -e "s/Cross .* = //"            \
-                    -e "s/%//"'                     \
-         ::: {-5..15..2}                            \
+parallel --plain                                                    \
+         --sshloginfile <(unused)                                   \
+         --filter-hosts                                             \
+         --retries 10                                               \
+         --timeout 28800                                            \
+         --tag                                                      \
+         'nice svm-train -q                                         \
+                         -m 1024                                    \
+                         -h 0                                       \
+                         -v 5                                       \
+                         -c $(echo 2^{1} | bc -l)                   \
+                         -g $(echo 2^{2} | bc -l)                   \
+                 "'$DATA'"                                          \
+              | sed -e "s/Cross .* = //"                            \
+                    -e "s/%//"'                                     \
+         ::: {-5..15..2}                                            \
          ::: {3..-15..-2}
